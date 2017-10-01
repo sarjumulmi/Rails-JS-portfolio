a37238738abb88de479c3289b9b198b3f7ad1f5b
sarjumulmi <mulmi.sarju@gmail.com>
2017-09-30 21:11:31 -0400

    survey helper methods added for survey#index page

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -1,2 +1,23 @@
 module SurveysHelper
+
+  def button_take_survey(survey)
+    link_to_if(user_signed_in?, "Take Survey", survey_take_survey_path(survey), :class => "btn btn-primary btn-sm")
+  end
+
+  def button_edit_survey(survey)
+    if user_signed_in? && survey.creator == current_user
+      link_to("Edit Survey", survey_path(survey), :class => "btn btn-primary btn-sm")
+    end
+  end
+
+  def button_survey_stat(survey)
+    if user_signed_in? && survey.creator == current_user
+      link_to("View Stat", survey_show_stat_path(survey), :class => "btn btn-primary btn-sm")
+    end
+  end
+
+  def button_create_survey(survey)
+    link_to_if(user_signed_in?, "Add New Survey", new_survey_path(@survey), :class => "btn btn-primary btn-lg")
+  end
+
 end
