4700bc8371661cbc811c49c13bae28c0de85322e
sarjumulmi <mulmi.sarju@gmail.com>
2017-11-11 13:10:01 -0500

    survey.js -> event handlers change to load on turbolinks:load

--- app/assets/javascripts/surveys.js
+++ app/assets/javascripts/surveys.js
@@ -1,19 +1,20 @@
-$(function(){
+$(document).on('turbolinks:load', function(){
   // event listener for Delete links for questions and answer options
   $('form').on('click','.remove-records', function(evt){
-    evt.preventDefault()
+
     $(this).prev('li').children('input[type=hidden]').val('1')
     $(this).closest('fieldset').hide()
+    evt.preventDefault()
+    // return false
     // debugger
   })
   // event listener for add fields for questions and answer options
   $('form').on('click', '.add_fields', function(evt){
-    evt.preventDefault()
-    // debugger
+    
     time = new Date().getTime()
     regexp = new RegExp($(this).data('id'), 'g')
 
-    // $('.fields').append($(this).data('fields').replace(regexp, time))
     $(this).parent().prev('.fields').append($(this).data('fields').replace(regexp, time))
+    evt.preventDefault()
   })
 })
